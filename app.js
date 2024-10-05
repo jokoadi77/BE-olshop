721a8009866d87686037689f040e1c15f282dbb1
Joko Adi S <user@USERs-MacBook-Air.local>
2024-10-06 02:16:58 +0700

    update file upload

--- app.js
+++ app.js
@@ -5,12 +5,21 @@ import { notFound, errorHandler } from './middlewares/errorMiddleware.js'
 import cookieParser from 'cookie-parser'
 import helmet from 'helmet'
 import ExpressMongoSanitize from 'express-mongo-sanitize'
+import { v2 as cloudinary } from 'cloudinary';
+
 
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT
+
+// Configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME , 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+});
 
 //middlewares
 app.use(express.json())
--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,7 @@
 import asyncHandler from "../middlewares/asyncHandler.js"; 
 import Product from "../models/productModel.js";
-
+import { v2 as cloudinary} from 'cloudinary'
+import streamifier from 'streamifier'
 
 export const createProduct = asyncHandler(async(req, res) => {
    const newProduct = await Product.create(req.body)
@@ -89,19 +90,24 @@ export const deleteProduct = asyncHandler(async(req, res) => {
 })
 
 export const fileUpload = asyncHandler(async(req, res) => {
-    const file = req.file;
-    if (!file) {
-        res.status(400)
-        throw new Error("no File Image uploaded")
-    }
-
-    const imageFileName = file.filename
-    const pathImageFile = `/uploads/${imageFileName}`
-
-    res.status(200).json({
-        message: "Image upload successfully",
-        image : pathImageFile
+  const stream = cloudinary.uploader.upload_stream({
+    folder: 'uploads',
+    allowed_formats: ['jpg', 'png']
+  },
+    function(err, result){
+        if (err) {
+            console.log(err);
+            return res.status(500).json({
+                message: "Failed upload image",
+                error: err
+            })
+            
+        }
+        res.json({message: "Image successfully uploaded",
+                 url: result.secure_url 
+        })
     })
+    streamifier.createReadStream(req.file.buffer).pipe(stream)
 })
 
 
